92cf83ca82c44ccaca7e9cd2ae66b8222c0b90b4
momma <momma.tatsunori@m.alhinc.jp>
2017-07-28 09:30:55 +0900

    first-commit

---
+++ src/main/java/jp/co/kenshu/form/SelectForm.java
@@ -0,0 +1,12 @@
+package jp.co.kenshu.form;
+
+public class SelectForm {
+	private String[] employees;
+
+	public String[] getEmployees() {
+		return employees;
+	}
+	public void setEmployees(String[] employees) {
+		this.employees = employees;
+	}
+}
